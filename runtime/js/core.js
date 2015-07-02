1bcc44544a6de9e7d441522b38fd12805ef87e4e
Tzvetan Mikov <tmikov@gmail.com>
2015-07-02 09:22:12 -0700

    runtime[core.js]: implement Function.prototype.call()

--- runtime/js/core.js
+++ runtime/js/core.js
@@ -5,3 +5,12 @@ function print(x) {
             'printf("%s\\n", %[x].raw.sval->getStr());'
    );
 }
+
+Function.prototype.call = function call (thisArg)
+{
+   __asm__({},[],[["thisArg", thisArg]],
+            'return argc > 1' +
+              '? js::call(&frame, argv[0], argc-1, argv+1)' +
+              ': js::call(&frame, argv[0], 1, &%[thisArg]);'
+   );
+}
