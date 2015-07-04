9b42226026ef94dc18f86052ef7587d880891a38
Tzvetan Mikov <tmikov@gmail.com>
2015-07-03 19:45:43 -0700

    runtime[core.js]: update for the latest __asm__ changes
    - clobbers parameter
    - %frame, %argc, %argv

--- runtime/js/core.js
+++ runtime/js/core.js
@@ -2,22 +2,22 @@ function print() {
     __asmh__({},"#include <stdio.h>");
     for ( var i = 0, e = arguments.length; i < e; ++i ) {
         if (i > 0)
-            __asm__({},[],[],"putchar(' ');");
-        __asm__({},[],[["x", arguments[i]]],
-                '%[x] = js::toString(&frame, %[x]);\n'+
+            __asm__({},[],[],[],"putchar(' ');");
+        __asm__({},[],[["x", arguments[i]]],[],
+                '%[x] = js::toString(%[%frame], %[x]);\n'+
                 'printf("%s", %[x].raw.sval->getStr());'
         );
     }
-    __asm__({},[],[],"putchar('\\n');");
+    __asm__({},[],[],[],"putchar('\\n');");
 }
 
 var console = { log: print };
 
 Function.prototype.call = function call (thisArg)
 {
-   __asm__({},[],[["thisArg", thisArg]],
-            'return argc > 1' +
-              '? js::call(&frame, argv[0], argc-1, argv+1)' +
-              ': js::call(&frame, argv[0], 1, &%[thisArg]);'
-   );
-}
+    return __asm__({},["result"],[["thisArg", thisArg]],[],
+        '%[result] = %[%argc] > 1' +
+            '? js::call(%[%frame], %[%argv][0], %[%argc]-1, %[%argv]+1)' +
+            ': js::call(%[%frame], %[%argv][0], 1, &%[thisArg]);'
+    );
+};
