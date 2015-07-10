7e8cc50999d310e748f0c291456a8298765446fc
Tzvetan Mikov <tmikov@gmail.com>
2015-07-09 20:46:43 -0700

    runtime[core.js]: add copyright notice

--- runtime/js/core.js
+++ runtime/js/core.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2015 Tzvetan Mikov.
+// Licensed under the Apache License v2.0. See LICENSE in the project
+// root for complete license information.
+
 function pr (x)
 {
     __asmh__({},"#include <stdio.h>");
