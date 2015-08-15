4778b9ceb1d5eff3bda99e746d59861296fab424
Tzvetan Mikov <tmikov@gmail.com>
2015-08-15 10:49:05 -0700

    runtime[process.js]: implement process.exit()

--- runtime/js/modules/process.js
+++ runtime/js/modules/process.js
@@ -2,3 +2,10 @@
 // Licensed under the Apache License v2.0. See LICENSE in the project
 // root for complete license information.
 
+
+exports.exit = function exit (code)
+{
+    __asm__({},[],[["code", code | 0]],[],
+        "::exit((int)%[code].raw.nval);"
+    );
+};
