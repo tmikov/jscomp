7535340d7942044647c41ecfb5addbac31d80941
Tzvetan Mikov <tmikov@gmail.com>
2015-08-22 13:04:27 -0700

    runtime[runtime.js]: implement parseInt()
    It is not a fully compliant implementation yet, but it is a start.

--- runtime/js/runtime.js
+++ runtime/js/runtime.js
@@ -18,6 +18,22 @@ function isFinite (number)
     );
 }
 
+function parseInt (string, radix)
+{
+    var inputString = String(string);
+    var R = radix === undefined ? 0 : radix | 0;
+
+    if (R !== 0 && (R < 2 || R > 36))
+        return NaN;
+
+    return __asm__({},["res"],[["inputString",inputString],["radix",radix]],[],
+        "const char * s = %[inputString].raw.sval->getStr();\n" +
+        "char * endptr;\n" +
+        "long val = strtol(s, &endptr, (int)%[radix].raw.nval);" +
+        "%[res] = js::makeNumberValue(endptr != s ? val : NAN);"
+    );
+}
+
 // SyntaxError
 //
 // NOTE: Error and TypeError are system-declared but the rest of the errors aren't
