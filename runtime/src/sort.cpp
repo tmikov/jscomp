f71b472675a31338d6eb511fa1c0ca2a4dfadb4f
Tzvetan Mikov <tmikov@gmail.com>
2015-08-27 21:13:22 -0700

    runtime: Array.sort: add median-of-three pivot selection

--- runtime/src/sort.cpp
+++ runtime/src/sort.cpp
@@ -33,19 +33,32 @@ void insertionSort (StackFrame * caller, IExchangeSortCB * cb, uint32_t begin, u
     }
 }
 
+// Must be at lest 3, for "median of three" to work
 #define INSERTION_THRESHOLD 8
 
 static void doQuickSort (StackFrame * caller, IExchangeSortCB * cb, uint32_t l, uint32_t r)
 {
 tail_recursion:
-    uint32_t i = l, j = r + 1;
-    // The pivot is at [l]
+    // Median-of-three
+    // Place the middle element at [l+1]
+    cb->swap(caller, l+1, l + ((r - l)>>1));
+    // Sort, [l], [l+1], [r]
+    if (cb->less(caller, r, l+1))
+        cb->swap(caller, r, l+1);
+    if (cb->less(caller, l+1, l))
+        cb->swap(caller, l+1, l);
+    if (cb->less(caller, r, l+1))
+        cb->swap(caller, r, l+1);
+    // Now [l] <= [l+1] <= [r]
+    // [l+1] is our pivot and [r] is a sentinel
+    uint32_t pivot = l+1;
+
+    uint32_t i = pivot, j = r + 1;
+    // The pivot is at [l+1]
     for(;;) {
-        while (cb->less(caller, ++i, l)) {
-            if (i == r)
-                break;
+        while (cb->less(caller, ++i, pivot)) {
         }
-        while (cb->less(caller, l, --j)) {
+        while (cb->less(caller, pivot, --j)) {
         }
         if (i >= j)
             break;
@@ -53,8 +66,8 @@ tail_recursion:
     }
 
     // put the pivot in its final position
-    if (j != l)
-        cb->swap(caller, l, j);
+    if (j != pivot)
+        cb->swap(caller, pivot, j);
 
     // To limit the stack size, recurse for the smaller partition and do tail-recursion for the bigger one
     uint32_t sl = j - l;
@@ -62,19 +75,27 @@ tail_recursion:
     if (sl <= sr) {
         if (sl > INSERTION_THRESHOLD)
             doQuickSort(caller, cb, l, j-1);
+        else
+            insertionSort(caller, cb, l, j);
+
         if (sr > INSERTION_THRESHOLD) {
-            //doQuickSort(caller, cb, j+1, r);
             l = j+1;
             goto tail_recursion;
+        } else {
+            insertionSort(caller, cb, j+1, r+1);
         }
 
     } else {
         if (sr > INSERTION_THRESHOLD)
             doQuickSort(caller, cb, j+1, r);
+        else
+            insertionSort(caller, cb, j+1, r+1);
+
         if (sl > INSERTION_THRESHOLD) {
-            //doQuickSort(caller, cb, l, j-1);
             r = j-1;
             goto tail_recursion;
+        } else {
+            insertionSort(caller, cb, l, j);
         }
     }
 }
@@ -83,7 +104,8 @@ void quickSort (StackFrame * caller, IExchangeSortCB * cb, uint32_t begin, uint3
 {
    if (end - begin > INSERTION_THRESHOLD)
        doQuickSort(caller, cb, begin, end-1);
-   insertionSort(caller, cb, begin, end);
+   else
+       insertionSort(caller, cb, begin, end);
 }
 
 }; // namespace js
