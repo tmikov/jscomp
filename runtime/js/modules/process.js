bf3038a46bd524a3af2b6af8134f34e7b6300996
Tzvetan Mikov <tmikov@gmail.com>
2015-08-30 15:12:44 -0700

    runtime: implement process.cwd()

--- runtime/js/modules/process.js
+++ runtime/js/modules/process.js
@@ -9,3 +9,49 @@ exports.exit = function exit (code)
         "::exit((int)%[code].raw.nval);"
     );
 };
+
+function strerror (errno)
+{
+    return __asm__({},["res"], [["errno", errno|0]], [],
+        "%[res] = js::makeStringValueFromASCII(%[%frame], ::strerror((int)%[errno].raw.nval));"
+    );
+}
+
+function throwIOError (errno, path, syscall)
+{
+    var msg = strerror(errno);
+    if (path)
+        msg = msg + " '" + path + "'";
+    var e = new Error(msg);
+    e.errno = errno;
+    //TODO: e.code = "???" // and also append it to the message
+    if (path !== undefined)
+        e.path = path;
+    if (syscall !== undefined)
+        e.syscall = syscall;
+
+    throw e;
+}
+
+__asmh__({},"#include <unistd.h>");
+__asmh__({},"#include <errno.h>");
+
+exports.cwd = function cwd ()
+{
+    var errno = 0;
+    var res = __asm__({},["res"],[["errno", errno]],[],
+        "char * s = ::getcwd(NULL, 0);\n" +
+        "if (s) {\n" +
+        "  %[errno] = js::makeNumberValue(0);\n" +
+        "  %[res] = js::makeStringValueFromUnvalidated(%[%frame], s);\n" +
+        "  ::free(s);\n" +
+        "} else {\n" +
+        "  %[errno] = js::makeNumberValue(errno);\n" +
+        "  %[res] = JS_NULL_VALUE;\n" +
+        "}"
+    );
+
+    if (res === null)
+        throwIOError(errno, undefined, "getcwd");
+    return res;
+};
