2e16a985c1e09fd1cb563afe7dd014eb7583c01d
Tzvetan Mikov <tmikov@gmail.com>
2015-08-31 21:16:49 -0700

    runtime[process]: implement process.argv and process.env
    The file names in process.argv are not yet resolved.

--- runtime/js/modules/process.js
+++ runtime/js/modules/process.js
@@ -2,6 +2,9 @@
 // Licensed under the Apache License v2.0. See LICENSE in the project
 // root for complete license information.
 
+exports.execArgv = null;
+exports.argv = null;
+exports.env = null;
 
 exports.exit = function exit (code)
 {
@@ -55,3 +58,60 @@ exports.cwd = function cwd ()
         throwIOError(errno, undefined, "getcwd");
     return res;
 };
+
+function initArgv ()
+{
+    var argc = __asm__({},["res"],[],[],"%[res] = js::makeNumberValue(JS_GET_RUNTIME(%[%frame])->argc)");
+    var argv = new Array(argc);
+
+    for ( var i = 0; i < argc; ++i ) {
+        argv[i] = __asm__({},["res"],[["i",i]], [],
+            "%[res] = js::makeStringValueFromUnvalidated(%[%frame], JS_GET_RUNTIME(%[%frame])->argv[(int)%[i].raw.nval]);"
+        );
+    }
+
+    var nodeArgv = new Array(argc + 1);
+    nodeArgv[0] = argv[0];
+    nodeArgv[1] = "";
+    for ( i = 1; i < argc; ++i )
+        nodeArgv[i+1] = argv[i];
+
+    exports.argv = nodeArgv;
+}
+
+function initEnv ()
+{
+    var env = {};
+    var name, value;
+    var i = 0;
+    for (;;) {
+        if (!__asm__({},["res"],[["i", i], ["name", name], ["value", value]],[],
+                "extern char ** environ;\n" +
+                "const char * line = environ[(int)%[i].raw.nval];\n" +
+                "if (line) {\n" +
+                "  const char * eq = ::strchr(line, '=');\n" +
+                "  if (eq) {\n" +
+                "    %[name] = js::makeStringValueFromUnvalidated(%[%frame], line, eq - line);\n" +
+                "    %[value] = js::makeStringValueFromUnvalidated(%[%frame], eq+1);\n" +
+                "  } else {\n" +
+                "    %[name] = js::makeStringValueFromUnvalidated(%[%frame], line);\n" +
+                "    %[value] = js::makeStringValue(JS_GET_RUNTIME(%[%frame])->permStrEmpty);\n" +
+                "  }\n" +
+                "  %[res] = js::makeBooleanValue(true);\n" +
+                "} else {\n" +
+                "  %[res] = js::makeBooleanValue(false);\n" +
+                "}"
+            ))
+        {
+            break;
+        }
+
+        env[name] = value;
+        ++i;
+    }
+
+    exports.env = env;
+}
+
+initArgv();
+initEnv();
