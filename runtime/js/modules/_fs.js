ff12bdd2abd8ec3f081a7b809fc9e5d7ddfaaa41
Tzvetan Mikov <tmikov@gmail.com>
2015-09-02 23:32:02 -0700

    runtime[_fs]: implement synchronous file open

--- runtime/js/modules/_fs.js
+++ runtime/js/modules/_fs.js
@@ -1,13 +1,26 @@
 // Copyright (c) 2015 Tzvetan Mikov.
 // Licensed under the Apache License v2.0. See LICENSE in the project
 // root for complete license information.
+var _jsc = require("./_jsc");
 
 exports.FSInitialize = function FSInitialize (stats) {
     console.error("process.binding.fs.FSInitialize() is not implemented");
 };
-exports.open = function close(path, flags, mode) {
-    console.error("process.binding.fs.open() is not implemented");
+
+exports.open = function open (path, flags, mode)
+{
+    var hnd = __asm__({},["res"],[["path", String(path)], ["flags", flags | 0], ["mode", mode | 0]], [],
+        "%[res] = js::makeNumberValue(" +
+            "::open(%[path].raw.sval->getStr()," +
+            "(int)%[flags].raw.nval," +
+            "(int)%[mode].raw.nval" +
+        "));"
+    );
+    if (hnd === -1)
+        _jsc.throwIOError("open", path);
+    return hnd;
 };
+
 exports.close = function close(fd) {
     console.error("process.binding.fs.close() is not implemented");
 };
